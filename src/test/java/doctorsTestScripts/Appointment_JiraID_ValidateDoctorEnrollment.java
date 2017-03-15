79db22bfc586ff19ae84b50e68c3dda018fe283e
Ganeshkumar.M <Z9149@AxisHO.AxisClinicals.Com>
2017-03-15 18:00:54 +0530

    Commiting the test case of doctor enrollment

---
+++ src/test/java/doctorsTestScripts/Appointment_JiraID_ValidateDoctorEnrollment.java
@@ -0,0 +1,105 @@
+package doctorsTestScripts;
+
+import java.io.File;
+
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+
+import objectRepository.PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+/*
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+*/
+import testBase.*;
+
+/*
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,
+MethodListener.class })
+
+*/
+public class Doc_JiraID_ValidateDoctorEnrollment extends LoadProp {
+	 public HomePage HomePageOfZoylo;
+	 public TestUtils baseActions;	
+
+	
+	
+		
+	 @BeforeClass
+    public void LaunchBrowser() throws Exception {
+  
+		  LoadBrowserProperties();
+		  HomePageOfZoylo= new HomePage(driver);
+		  baseActions= new TestUtils(driver);
+		 
+		 
+ }
+
+	 
+	 @DataProvider(name = "DP1")
+	    public Object[][] createData_DP1() throws Exception{
+	        Object[][] retObjArr=TestUtils.getTableArray("TestData\\Data.xls","Doctor", "TC2");
+	        return(retObjArr);
+	    }
+	 @Test(dataProvider="DP1")
+	 public void test1(String runmode,String Area, String FirstName,String LastName,String Gender,String Qualification,String Email, String Address,String Fee,String Notes,String expected) throws Exception {
+	  
+		 if(runmode.equals("yes")){
+			 
+		
+			 
+			 
+			 //Test Starts - Here
+			 baseActions.openUrl("https://zoyloqa.zoylo.com/doctorenrollmentform");
+			 HomePageOfZoylo.doctorsEnrollment( Area,  FirstName, LastName, Gender, Qualification, Email,  Address, Fee, Notes);
+			 Thread.sleep(5000);
+			 String SuccessfulText=driver.findElement(By.xpath("//h5")).getText();
+			 System.out.println("SuccessfulText="+SuccessfulText);
+			//Comparing Actual VS Expected
+			 Assert.assertTrue(SuccessfulText.contains(expected)); 
+			
+			
+		
+			
+			
+		 }else{
+			 
+			throw new SkipException("RUNMODE IS OFF");
+			
+		 }
+			
+			
+	    }
+    
+	 
+	 
+	 
+	 
+	 
+	 @AfterClass
+	 
+	 public void Exit() {
+
+	       
+	       driver.close();
+	       
+	      
+	    }
+    
+	
+
+    
+    
+}
