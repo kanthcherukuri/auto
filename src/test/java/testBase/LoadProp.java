a48df098300543dcf47c4a833d6e163ef332ad9d
Ganeshkumar.M <Z9149@AxisHO.AxisClinicals.Com>
2017-03-15 16:48:41 +0530

    comiting the load prop

---
+++ src/test/java/testBase/LoadProp.java
@@ -0,0 +1,44 @@
+package testBase;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import objectRepository.PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoadProp extends PageObjects  {
+	 public static String driverpath;
+	 public static String base_url,browser_name , login_username,login_password,namespaceurl ;
+	
+		public static WebDriver driver;
+	public static WebDriver LoadBrowserProperties()throws Exception{
+	    FileInputStream inStream;
+        inStream = new FileInputStream(new File("ConfigFiles\\Setup-Details.txt"));
+        Properties prop = new Properties();
+        prop.load(inStream);
+        driverpath=prop.getProperty("driver.path");
+        base_url=prop.getProperty("base.url");
+        browser_name=prop.getProperty("browser.name");
+        login_username=prop.getProperty("login.username");
+        login_password=prop.getProperty("login.password");	
+        
+        
+        
+       
+        
+        if(browser_name.equals("chrome")){
+			System.out.println("launching chrome browser");
+			System.setProperty("webdriver.chrome.driver", driverpath+"chromedriver.exe");
+			driver = new ChromeDriver();
+		}else if(browser_name.equals("firefox")){
+			System.out.println("launching Firefox browser");
+			driver = new FirefoxDriver();	
+		}
+		return driver;
+}
+
+}
