edf448b579374a1a0720063c8b1d6cf16231a67b
Ganesh M <ganeshkumar.m@zoylo.com>
2017-03-17 13:22:53 +0530

    Commit the classname in elements diagnostics

--- src/test/java/objectRepository/Elements_Diagnostics.java
+++ src/test/java/objectRepository/Elements_Diagnostics.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class PageObjects {
+public class Elements_Diagnostics {
 	
 	public static Properties prop = new Properties();
 	public static FileInputStream inStream;
