1bc9a0f5fde20ded0ef52420d555fb22dbe93636
Ganesh M <ganeshkumar.m@zoylo.com>
2017-03-27 15:50:28 +0530

    Adding new test case 1092

---
+++ src/test/java/recipientsTestScripts/Recipients_ZOY1092_SearchByDoctorSpecializationNClinic.java
@@ -0,0 +1,121 @@
+package recipientsTestScripts;
+
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+
+
+
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+/*
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+*/
+import testBase.*;
+import objectRepository.*;
+
+/*
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,
+MethodListener.class })
+
+*/
+public class Recipients_ZOY1092_SearchByDoctorSpecializationNClinic extends LoadProp {
+	 public RecipientPage RecipientPage;
+	 public TestUtils Browser;	
+
+	
+	
+		
+	 @BeforeClass(groups = { "Regression","High" })	
+    public void LaunchBrowser() throws Exception {
+  
+		  LoadBrowserProperties(); // Create driver instance and launch the browser
+		  Elements_Recipients.Recipients_PageProperties();// loading UI Page Elements / Locators
+		  RecipientPage = new RecipientPage(driver); // Loading Pages
+		  Browser= new TestUtils(driver);   
+		  //Test Starts-Here
+		  Browser.openUrl(recipient_url);			
+	      //Verify Recipient Login with valid details
+		  RecipientPage.recipientLogin(Recipient_Username, Recipient_Password);
+		  Thread.sleep(2000);
+		  	 
+ } 
+
+ 
+	 @DataProvider(name = "DP1")
+	    public Object[][] createData_DP1() throws Exception{
+	        Object[][] retObjArr=TestUtils.getTableArray("TestData\\Recipients_TestData.xls","Doctor", "ZOY1092");
+	        return(retObjArr);
+	    }
+	 @Test(dataProvider="DP1",groups = { "Regression","Medium" })
+	 public void SearchByDoctorSpecializationNClinic(String runmode,String Doctor,String Specialization,String Clinic,String invalidData ) throws Exception {
+	  
+		 if(runmode.equals("yes")){
+			 			 
+			//Searching Locality/Area
+			RecipientPage.searchInZoyloMAPArea("Hyderabad");
+			//Verify Doctors
+			RecipientPage.searchInZoyloMAP(Doctor);
+			String Search_Doctor = driver.findElement(By.xpath("//h1")).getText();
+	        Assert.assertTrue(Search_Doctor.contains(Doctor));
+	        RecipientPage.goToDoctors();
+	   	    //Verify Specialization
+			RecipientPage.searchInZoyloMAP(Specialization);
+			String Search_Specialization = driver.findElement(By.xpath("//div[@class='dctr-desig']")).getText();
+	        Assert.assertTrue(Search_Specialization.contains(Specialization));
+	        System.out.println("Passed"+Search_Specialization);
+	        RecipientPage.goToDoctors();
+	        //Verify Clinic
+	        driver.navigate().refresh();
+			RecipientPage.searchInZoyloMAP(Clinic);
+			RecipientPage.bookAppointment();
+			String Search_Clinic = driver.findElement(By.xpath("//h2[@class='addr-ClinicName']")).getText();
+			System.out.println("clicnic name"+Search_Clinic);
+	        Assert.assertEquals(Search_Clinic, Clinic);
+	       
+	        
+	        //Verify with Invalid data
+	        RecipientPage.goToDoctors();
+	        driver.findElement(By.id("search2")).click();
+	    	driver.findElement(By.id("indexSearchTextbox")).sendKeys(invalidData);
+	    	Thread.sleep(5000);
+	    	String OppsContent = driver.findElement(By.cssSelector("div.a-s-w > span")).getText();
+	    	Assert.assertEquals(OppsContent, "Oops! your search for "+invalidData+" did not match any records");
+	        
+			
+			 
+		 }else{
+			 
+			throw new SkipException("RUNMODE IS OFF");
+			
+		 }
+			
+			
+	    }
+    
+	 
+	 
+	 
+	 
+	 
+	 @AfterClass(groups = { "Regression","High" })
+	 
+	 public void Exit() {
+
+	       
+	       driver.close();
+	       
+	      
+	    }
+    
+	
+
+    
+    
+}
