340eb6f53b174078280b60c34b4c0707057ac22f
GaneshKumarMandala <ganeshkumar.m@Zoylo.com>
2017-04-13 16:06:38 +0530

    commiting the Performance scripts

---
+++ src/test/java/performanceTestScripts/ZoyloHomePage.java
@@ -0,0 +1,68 @@
+package performanceTestScripts;
+
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+
+
+
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.*;
+/*
+import atu.testng.reports.listeners.ATUReportsListener;
+import atu.testng.reports.listeners.ConfigurationListener;
+import atu.testng.reports.listeners.MethodListener;
+*/
+import testBase.*;
+import objectRepository.*;
+
+/*
+@Listeners({ ATUReportsListener.class, ConfigurationListener.class,
+MethodListener.class })
+
+*/
+public class ZoyloHomePage extends LoadPropMac {
+	 public RecipientPage RecipientPage;
+	 public TestUtils Browser;	
+
+	
+	
+		
+	 @BeforeClass(groups = { "Regression","High" })	
+    public void LaunchBrowser() throws Exception {
+  
+		  LoadBrowserProperties(); // Create driver instance and launch the browser
+		  Elements_Recipients.Recipients_PageProperties();// loading UI Page Elements / Locators
+		  RecipientPage = new RecipientPage(driver); // Loading Pages
+		  Browser= new TestUtils(driver);        
+		  	 
+ } 
+
+	 @Test(groups = { "Regression","High" })
+	 public void LandingPageOfZoylo( ) throws Exception {
+	 
+			    //Test Starts-Here
+		        driver.get(base_url);	
+		        
+			
+	    }
+
+	 @AfterClass(groups = { "Regression","High" })
+	 
+	 public void Exit() {
+
+	       
+	       driver.close();
+	       
+	      
+	    }
+    
+	
+
+    
+    
+}
