82a5401b80b9d118bc08eb560e3968d7e3cb843c
LAKSHMIKANTH <lakshmikanth.c@zoylo.com>
2017-06-06 09:22:23 +0530

    Changes Updated

--- src/test/java/testBase/DiagnosticPage.java
+++ src/test/java/testBase/DiagnosticPage.java
@@ -13,15 +13,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.asserts.SoftAssert;
-
-import testBase.*;
 
 import objectRepository.Elements_Diagnostics;
 
