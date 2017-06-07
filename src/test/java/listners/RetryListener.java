b87e5bffa4be401b3cd70a0b2cd2458756433294
LAKSHMIKANTH <lakshmikanth.c@zoylo.com>
2017-06-07 09:50:43 +0530

    Changes updated

--- src/test/java/listners/RetryListener.java
+++ src/test/java/listners/RetryListener.java
@@ -12,6 +12,7 @@ public class RetryListener implements IAnnotationTransformer{
 	@Override
 	public void transform(ITestAnnotation testannotation, Class testClass,
 			Constructor testConstructor, Method testMethod)	{
+		
 		IRetryAnalyzer retry = testannotation.getRetryAnalyzer();
 
 		if (retry == null)	{
