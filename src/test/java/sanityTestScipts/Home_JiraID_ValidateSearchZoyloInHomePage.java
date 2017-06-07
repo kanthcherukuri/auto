2575a95afd1dd5368afc3e6bc940968d0f1d71be
Sen <sagarsen.c@zoylo.com>
2017-06-07 13:42:31 +0530

    ZOY-2121 removed warnings

--- src/test/java/sanityTestScipts/Home_JiraID_ValidateSearchZoyloInHomePage.java
+++ src/test/java/sanityTestScipts/Home_JiraID_ValidateSearchZoyloInHomePage.java
@@ -1,14 +1,7 @@
 package sanityTestScipts;
 
-
-
 import org.openqa.selenium.*;
-
-
-
-
 import org.testng.Assert;
-import org.testng.Reporter;
 import org.testng.SkipException;
 import org.testng.annotations.*;
 /*
