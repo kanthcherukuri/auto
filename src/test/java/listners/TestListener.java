f1c4ebc5465bf8c2977eec0bcc3626189bf0bb4e
LAKSHMIKANTH <lakshmikanth.c@zoylo.com>
2017-06-07 11:28:35 +0530

    Changes Updated

--- src/test/java/listners/TestListener.java
+++ src/test/java/listners/TestListener.java
@@ -3,14 +3,11 @@ package listners;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
-import java.util.Set;
-
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
-import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 
 import testBase.LoadPropMac;
