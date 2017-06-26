9242a68ac3b76134e2d5b5b0ec2c5b064ce9399b
Sen <sagarsen.c@zoylo.com>
2017-06-26 11:32:06 +0530

    ZOY-2121 changes to ZOY815 scripts completed

--- src/test/java/doctorsTestScripts/Schedule_ZOY815_Doctor_EditTimeSlotForClinics_IncreaseTime.java
+++ src/test/java/doctorsTestScripts/Schedule_ZOY815_Doctor_EditTimeSlotForClinics_IncreaseTime.java
@@ -52,7 +52,7 @@ public class Schedule_ZOY815_Doctor_EditTimeSlotForClinics_IncreaseTime extends 
 		Thread.sleep(1000);
 		driver.findElement(By.xpath(Elements_Doctors.WendTime)).sendKeys(updtendTime);
 		driver.findElement(By.xpath(Elements_Doctors.clinicSubmitTimeSlots)).click(); //Save
-		Browser.CheckNotificationMessage("Schedule Updated Successfully");
+		Browser.CheckNotificationMessage("Clinic Time Slot Updated Successfully");
 		Thread.sleep(3000);
 		doctorsPage.cancelSundayAppt(); //cancel sunday appointment
 		Thread.sleep(4000);
