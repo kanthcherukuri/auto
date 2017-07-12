1eca6d068664db8e76cb0191acbfa04dbd7e00b5
Sen <sagarsen.c@zoylo.com>
2017-07-12 16:39:34 +0530

    ZOY-2278 Z2100 deactivate all break timings code added

--- src/test/java/doctorsTestScripts/Schedule_ZOY2100_AddClinic.java
+++ src/test/java/doctorsTestScripts/Schedule_ZOY2100_AddClinic.java
@@ -1,7 +1,11 @@
 package doctorsTestScripts;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+
+import objectRepository.Elements_Doctors;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import testBase.DoctorsPage;
@@ -34,6 +38,36 @@ public class Schedule_ZOY2100_AddClinic extends LoadPropMac
 		launchapp();
 		admin.adminSignIn(admin_user, admin_password);
 		doctorsPage.deleteOtherClinicFromAdmin(DoctorsLogin_username);
+		
+		//Admin logout
+		Thread.sleep(6000);
+		Browser.waitFortheElementXpath("//button[@class='btn btn-default dropdown-toggle']");
+		driver.findElement(By.xpath("//button[@class='btn btn-default dropdown-toggle']")).click();
+		Thread.sleep(1000);
+		driver.findElement(By.id("logout")).click();
+		Browser.waitFortheElementXpath("//img[@class='indexProfileImg']");
+		
+		//Deactivate all days break time temporary error cover methods
+		driver.get(loginPage_Url);
+		doctorsPage.SignIn(DoctorsLogin_username, DoctorsLogin_password);
+		Thread.sleep(2000);
+		driver.findElement(By.id(Elements_Doctors.schedule)).click();
+		Browser.waitforTextbyxpath("(//div[@class='day-title'])[1]", "Consultation");
+		Browser.scrollbyxpath(Elements_Doctors.submitSchedule);
+		driver.findElement(By.xpath(Elements_Doctors.submitSchedule)).click();
+		Browser.waitFortheElementXpath("//div[@class='zy-status-wrapper']");
+		String verifyMsg=Browser.getTextByXpath("//div[@class='zy-status-wrapper']");
+		System.out.println(verifyMsg);
+		Thread.sleep(6000);
+		if(verifyMsg.equalsIgnoreCase("Start time is mandatory"))
+		{
+			for(int i=1;i<=7;i++)
+			{
+				driver.findElement(By.xpath("(//label[@class='sp-doc-conc-work-hours-switch-label'])["+i+"]")).click();
+			}
+		}
+		driver.findElement(By.xpath(Elements_Doctors.submitSchedule)).click();
+		Browser.CheckNotificationMessage("Schedule updated successfully");
 	}
 	
 	@BeforeClass
