c4a6a4736b6cb5a16350482b9adbeb1cc37f8b44
Sen <sagarsen.c@zoylo.com>
2017-07-18 17:24:03 +0530

    ZOY-2418 script completed

---
+++ src/test/java/diagnosticTestScripts/Profile_ZOY2418_ProfilePictureFunctionality.java
@@ -0,0 +1,86 @@
+package diagnosticTestScripts;
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import objectRepository.Elements_Diagnostics;
+import objectRepository.Elements_Doctors;
+import objectRepository.Elements_NewAdminDoctors;
+import objectRepository.Elements_Recipients;
+import testBase.DiagnosticPage;
+import testBase.LoadPropMac;
+import testBase.RecipientPage;
+import testBase.TestUtils;
+
+//@Author: Sagar Sen
+
+public class Profile_ZOY2418_ProfilePictureFunctionality extends LoadPropMac
+{
+	public TestUtils Browser;
+	public RecipientPage RecipientPage;
+	public DiagnosticPage diagnosticPage;
+	
+	@Test(priority=1)
+	public void diagnosticProfileTakePhoto() throws Exception
+	{
+		Browser.waitFortheID(Elements_Diagnostics.clickondashboardmenu);
+		Browser.scrollbyID(Elements_Diagnostics.clickonaccountmenu);
+		Browser.clickOnTheElementByID(Elements_Diagnostics.clickonaccountmenu);
+		diagnosticPage.goToDcProfilePicture();
+		Browser.clickOnTheElementByXpath(Elements_Doctors.doctor_profileTakePhotoBtn);
+		if(driver.findElements(By.id(Elements_Doctors.doctor_profileVideoWindow)).size()!=0)
+		{
+			System.out.println("The video window is available to take a picture");
+			Browser.clickOnTheElementByXpath(Elements_Doctors.doctor_profileVideoWindowTakePhotoBtn);
+			Browser.clickOnTheElementByXpath(Elements_Doctors.doctor_profileVideoUsePhotoBtn);
+		}
+		else
+		{
+			System.out.println("The video window is not available to take a picture");
+		}
+		Browser.CheckNotificationMessage("Profile photo uploaded successfully");
+		Thread.sleep(5000);
+	}
+	
+	@Test(priority=2)
+	public void diagnosticProfileUploadPhoto() throws Exception
+	{
+		driver.navigate().refresh();
+		diagnosticPage.goToDcProfilePicture();
+		Browser.enterTextByID(Elements_Doctors.doctor_profileUploadInputID, System.getProperty("user.dir")+"/Uploads/d.png");
+		Browser.CheckNotificationMessage("Profile photo changed successfully");
+		Thread.sleep(5000);
+	}
+	
+	@Test(priority=3)
+	public void diagnosticProfileRemovePhoto() throws Exception
+	{
+		driver.navigate().refresh();
+		diagnosticPage.goToDcProfilePicture();
+		Browser.clickOnTheElementByXpath(Elements_Diagnostics.dc_profilePictureRemoveBtn);
+		Browser.CheckNotificationMessage("Your profile photo has been successfully removed");
+		Thread.sleep(5000);
+		diagnosticPage.diagnosticslogout(); //logout
+	}
+	
+	@BeforeClass
+	public void launchbrowser() throws Exception
+	{
+		LoadBrowserProperties();
+		Elements_NewAdminDoctors.newAdmin_DoctorPageProperties(); // loading the Elements
+		Elements_Recipients.Recipients_PageProperties();
+		Elements_Doctors.Doc_PageProperties();
+		Elements_Diagnostics.Diag_PageProperties();
+		Browser= new TestUtils(driver);
+		RecipientPage=new RecipientPage(driver);
+		diagnosticPage=new DiagnosticPage(driver);
+		driver.get(loginPage_Url);
+		diagnosticPage.SignIn(recipientDC_User, recipientDC_Password);
+	}
+	
+	@AfterClass
+	public void Exit() 
+	{	
+		driver.quit();
+	} 
+}
--- src/test/java/testBase/RecipientPage.java
+++ src/test/java/testBase/RecipientPage.java
@@ -1,7 +1,4 @@
 package testBase;
-
-
-
 import objectRepository.*;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
@@ -10,8 +7,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.SkipException;
 
@@ -218,7 +213,12 @@ public class RecipientPage  {
 	 *  @Parameters  : DCName
 	 *  @Return      : 
 	 */
-	public void searchDCInZoyloMAP(String keyword) throws InterruptedException{
+	public void searchDCInZoyloMAP(String keyword) throws InterruptedException
+	{
+		if(driver.findElements(By.xpath(Elements_Home.Map_DiagnosticsCenters)).size()==0)
+		{
+			Browser.clickOnTheElementByID("diagnostics");
+		}
 		Browser.waitFortheElementXpath(Elements_Home.Map_DiagnosticsCenters);
 		Thread.sleep(1000);
 		driver.findElement(By.id("search2")).click();
@@ -231,7 +231,6 @@ public class RecipientPage  {
 	    Thread.sleep(500);
 		driver.findElement(By.cssSelector("div.a-s-w > span")).click();
 		Thread.sleep(2000);
-
 	}
 	
 	/*   
