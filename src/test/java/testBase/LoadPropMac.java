d3b0bf16f7cba120ff96c24fc114fe6ed0b157c2
GaneshKumarMandala <ganeshkumar.m@Zoylo.com>
2017-07-27 15:28:27 +0530

    new props

--- src/test/java/testBase/LoadPropMac.java
+++ src/test/java/testBase/LoadPropMac.java
@@ -149,7 +149,7 @@ public class LoadPropMac   {
 		if(browser_name.equals("chrome")){
 			//String os = System.getProperty("os.name").toLowerCase(); // Added to verify the OS
 			System.out.println("launching chrome browser");
-			System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriver");
+			System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriverL");
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("disable-infobars");                   // Added to remove new chrome warning message
 			options.addArguments("--use-fake-ui-for-media-stream=1");   // Added to allow camera
