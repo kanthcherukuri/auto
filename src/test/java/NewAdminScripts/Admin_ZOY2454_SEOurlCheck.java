13c757401623bcaeb9bee1b7bcac0bd2fcdb7eec
Sen <sagarsen.c@zoylo.com>
2017-08-31 16:29:18 +0530

    ZOY-2630 script completed

--- src/test/java/NewAdminScripts/Admin_ZOY2454_SEOurlCheck.java
+++ src/test/java/NewAdminScripts/Admin_ZOY2454_SEOurlCheck.java
@@ -18,6 +18,7 @@ public class Admin_ZOY2454_SEOurlCheck extends LoadPropMac
 	public NewAdminDoctorsPage admin;
 	public String emailID="niru2pama@yahoo.com";
 	public String seoID="dr-nirupama-dentist-in-adikmet";
+	public String specURL="doctors/seospeciality";
 	
 	@Test(priority=1)
 	public void activeDoctorSEOurl() throws Exception
@@ -35,7 +36,7 @@ public class Admin_ZOY2454_SEOurlCheck extends LoadPropMac
 		Browser.openUrl("https://"+Environment_Name+".zoylo.com/generateSitemap"); //Generate sitemap to update SEO collections
 		Thread.sleep(5000);
 		driver.navigate().refresh();
-		Thread.sleep(20000);
+		Thread.sleep(5000);
 		Browser.openUrl("https://"+Environment_Name+".zoylo.com/sitemap/cityDoctor-1-sitemap.xml");
 		Thread.sleep(5000);
 		boolean reader = driver.getPageSource().contains(seoID);
@@ -61,10 +62,60 @@ public class Admin_ZOY2454_SEOurlCheck extends LoadPropMac
 		Browser.openUrl("https://"+Environment_Name+".zoylo.com/generateSitemap"); //Generate sitemap to update SEO collections
 		Thread.sleep(5000);
 		driver.navigate().refresh();
-		Thread.sleep(20000);
+		Thread.sleep(5000);
 		Browser.openUrl("https://"+Environment_Name+".zoylo.com/sitemap/cityDoctor-1-sitemap.xml");
 		Thread.sleep(5000);
 		boolean reader = driver.getPageSource().contains(seoID);
+		assertFalse(reader);
+	}
+	
+	@Test(priority=3)
+	public void activeSpecialityURL() throws Exception
+	{
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/login");
+		admin.click_doctorsTab();
+		admin.searchDoctorbyEmailID(emailID);
+		boolean isChecked1 = driver.findElement(By.xpath(Elements_NewAdminDoctors.doctorActiveCheckBox)).isSelected();
+		System.out.println(emailID + "" + isChecked1);
+		if(isChecked1==false)
+		{
+			Browser.clickOnTheElementByXpath(Elements_NewAdminDoctors.doctorActiveCheckBox);
+		}
+		boolean isChecked2 = driver.findElement(By.xpath(Elements_NewAdminDoctors.doctorActiveCheckBox)).isSelected();
+		System.out.println(emailID + "" + isChecked2);
+		Thread.sleep(2000);
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/generateSitemap"); //Generate sitemap to update SEO collections
+		Thread.sleep(5000);
+		driver.navigate().refresh();
+		Thread.sleep(5000);
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/sitemap/citySpeciality-1-sitemap.xml");
+		Thread.sleep(5000);
+		boolean reader = driver.getPageSource().contains(specURL);
+		assertTrue(reader);
+	}
+	
+	@Test(priority=4)
+	public void inactiveSpecialityURL() throws Exception
+	{
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/login");
+		admin.click_doctorsTab();
+		admin.searchDoctorbyEmailID(emailID);
+		boolean isChecked1 = driver.findElement(By.xpath(Elements_NewAdminDoctors.doctorActiveCheckBox)).isSelected();
+		System.out.println(emailID + "" + isChecked1);
+		if(isChecked1==true)
+		{
+			Browser.clickOnTheElementByXpath(Elements_NewAdminDoctors.doctorActiveCheckBox);
+		}
+		boolean isChecked2 = driver.findElement(By.xpath(Elements_NewAdminDoctors.doctorActiveCheckBox)).isSelected();
+		System.out.println(emailID + "" + isChecked2);
+		Thread.sleep(2000);
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/generateSitemap"); //Generate sitemap to update SEO collections
+		Thread.sleep(5000);
+		driver.navigate().refresh();
+		Thread.sleep(5000);
+		Browser.openUrl("https://"+Environment_Name+".zoylo.com/sitemap/citySpeciality-1-sitemap.xml");
+		Thread.sleep(5000);
+		boolean reader = driver.getPageSource().contains(specURL);
 		assertFalse(reader);
 	}
 	
