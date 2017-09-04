9fc2b4e98761ed7038a51a5ae01795658830e260
Sen <sagarsen.c@zoylo.com>
2017-09-04 12:51:42 +0530

    test commit from katalon studio

---
+++ Scripts/Recipient_Login_Logout_Android/Script1504509418919.groovy
@@ -0,0 +1,39 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('/Users/sen/Desktop/apps/zoylo-PIT-24Apr.apk', true)
+
+Mobile.tap(findTestObject('myAccountFooter'), 0)
+
+Mobile.setText(findTestObject('EmailID'), 'sagarsen.c@zoylo.com', 0)
+
+Mobile.setText(findTestObject('Password'), 'Zoylo@123', 0)
+
+Mobile.tap(findTestObject('loginButton'), 0)
+
+Thread.sleep(5000)
+
+Mobile.tap(findTestObject('myAccountHeader'), 0)
+
+Mobile.tap(findTestObject('logoutHeader'), 0)
+
+Mobile.closeApplication()
+
