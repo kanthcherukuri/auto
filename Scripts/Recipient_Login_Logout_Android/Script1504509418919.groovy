378ae46850afc5cf586602dbc4cc0e71406b1cf0
Sen <sagarsen.c@zoylo.com>
2017-09-04 14:25:28 +0530

    changes to wait

--- Scripts/Recipient_Login_Logout_Android/Script1504509418919.groovy
+++ Scripts/Recipient_Login_Logout_Android/Script1504509418919.groovy
@@ -4,7 +4,6 @@ import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
 import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
 import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
 import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
-import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
 import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
 import com.kms.katalon.core.model.FailureHandling as FailureHandling
 import com.kms.katalon.core.testcase.TestCase as TestCase
@@ -29,11 +28,10 @@ Mobile.setText(findTestObject('Password'), 'Zoylo@123', 0)
 
 Mobile.tap(findTestObject('loginButton'), 0)
 
-Thread.sleep(5000)
+Thread.sleep(50000)
 
 Mobile.tap(findTestObject('myAccountHeader'), 0)
 
 Mobile.tap(findTestObject('logoutHeader'), 0)
 
 Mobile.closeApplication()
-
