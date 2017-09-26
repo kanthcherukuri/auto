58846a5b3db279b99433668574f66c15f50384a9
GaneshKumarMandala <ganeshkumar.m@Zoylo.com>
2017-09-26 12:34:25 +0530

    Added headless arguments in load prop

--- src/test/java/testBase/LoadPropMac.java
+++ src/test/java/testBase/LoadPropMac.java
@@ -161,7 +161,7 @@ public class LoadPropMac   {
 			System.out.println("launching chrome browser");
 			System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriver");
 			ChromeOptions options = new ChromeOptions();
-			//options.addArguments("--headless");                       //Added to launch chrome without GUI
+			options.addArguments("--headless");                       //Added to launch chrome without GUI
 			options.addArguments("disable-infobars");                   // Added to remove new chrome warning message
 			options.addArguments("--use-fake-ui-for-media-stream=1");   // Added to allow camera
 			//options.addArguments("--kiosk");                          // Added to Maximize window
