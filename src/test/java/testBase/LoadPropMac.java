55186ef284b7f72fcf517f2c751eab7d2e4dfd16
GaneshKumarMandala <ganeshkumar.m@Zoylo.com>
2017-09-27 15:58:46 +0530

    new chrome Linux

--- src/test/java/testBase/LoadPropMac.java
+++ src/test/java/testBase/LoadPropMac.java
@@ -159,7 +159,7 @@ public class LoadPropMac   {
 		if(browser_name.equals("chrome")){
 			//String os = System.getProperty("os.name").toLowerCase(); // Added to verify the OS
 			System.out.println("launching chrome browser");
-			System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriver");			
+			System.setProperty("webdriver.chrome.driver", "BrowserDrivers/chromedriverL");			
 			ChromeOptions options = new ChromeOptions();
 			options.addArguments("--headless");                       //Added to launch chrome without GUI
 			options.addArguments("disable-infobars");                   // Added to remove new chrome warning message
